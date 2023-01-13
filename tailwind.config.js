fc8744e067d32df0fcfb04b65d1b77240adc6b9b
TheAnnoying <drork2018@gmail.com>
2023-01-13 21:20:57 +0200

    Formatted the tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,36 +1,36 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
-  daisyui: {
-    themes: [
-      {
-        mclinker: {
-          "primary": "#729e34",
-          "secondary": "#BF95F9",
-          "accent": "#FFB86B",
-          "neutral": "#222e35",
-          "base-100": "#111b21",
-          "info": "#8BE8FD",
-          "success": "#52F17C",
-          "warning": "#F1FA89",
-          "error": "#FF5757"
-        },
-        mclinkerlight: {
-          "primary": "#729e34",
-          "secondary": "#BF95F9",
-          "accent": "#FFB86B",
-          "neutral": "#f0f2f5",
-          "base-100": "#e7e4df",
-          "info": "#8BE8FD",
-          "success": "#52F17C",
-          "warning": "#F1FA89",
-          "error": "#FF5757"
-        }
-      },
-    ],
-  },
-  theme: {
-    extend: {},
-  },
-  plugins: [require("daisyui")],
+    content: ["./src/**/*.{html,js}"],
+    daisyui: {
+        themes: [
+            {
+                mclinker: {
+                    "primary": "#729e34",
+                    "secondary": "#BF95F9",
+                    "accent": "#FFB86B",
+                    "neutral": "#222e35",
+                    "base-100": "#111b21",
+                    "info": "#8BE8FD",
+                    "success": "#52F17C",
+                    "warning": "#F1FA89",
+                    "error": "#FF5757"
+                },
+                mclinkerlight: {
+                    "primary": "#729e34",
+                    "secondary": "#BF95F9",
+                    "accent": "#FFB86B",
+                    "neutral": "#f0f2f5",
+                    "base-100": "#e7e4df",
+                    "info": "#8BE8FD",
+                    "success": "#52F17C",
+                    "warning": "#F1FA89",
+                    "error": "#FF5757"
+                }
+            },
+        ],
+    },
+    theme: {
+        extend: {},
+    },
+    plugins: [require("daisyui")]
 }

d73f86dcd365a671aa178272851d58b1bf88dbb4
TheAnnoying <drork2018@gmail.com>
2023-01-22 15:25:54 +0200

    Added javascript for the countdown and redirection

--- src/index.js
+++ src/index.js
@@ -8,3 +8,12 @@ let observer = new IntersectionObserver((entries, observer) => {
 
 const hiddenElements = document.querySelectorAll('.unshown');
 hiddenElements.forEach((el) => observer.observe(el));
+
+if(document.URL.includes("invite")){
+    let counter = 3;
+    setInterval(() => {
+        counter > 0 ? counter-- : null;
+        counter == 0 ? window.location.replace("https://discord.com/oauth2/authorize?client_id=712759741528408064&permissions=275415092224&scope=applications.commands%20bot") : null;
+        document.getElementById("counterElement").style.setProperty('--value', counter);
+    }, 1000);
+}

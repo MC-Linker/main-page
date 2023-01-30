329390ee3de045b1d26e0811727673ae97d69624
TheAnnoying <drork2018@gmail.com>
2023-01-30 20:15:04 +0200

    Changed threshold (again)

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ let observer = new IntersectionObserver((entries, observer) => {
             entry.target.classList.add("shown");
         }
     }
-}, { threshold: [0, 0.25, 0.5, 0.75, 1] });
+}, { threshold: 1 });
 
 const hiddenElements = document.querySelectorAll('.unshown');
 hiddenElements.forEach((el) => observer.observe(el));

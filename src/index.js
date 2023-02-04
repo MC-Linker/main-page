43f819b7548a7b064f547e10519829397f4f5a72
Lianecx <84291452+Lianecx@users.noreply.github.com>
2023-02-04 17:12:52 +0100

    Fixes + Organization

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 
 
-let observer = new IntersectionObserver((entries, observer) => {
+let observer = new IntersectionObserver(entries => {
     for (const entry of entries) {
         if (entry.intersectionRatio === 1) {
             entry.target.classList.add("shown");
@@ -9,14 +9,14 @@ let observer = new IntersectionObserver((entries, observer) => {
 }, { threshold: 1 });
 
 const hiddenElements = document.querySelectorAll('.unshown');
-hiddenElements.forEach((el) => observer.observe(el));
+hiddenElements.forEach(el => observer.observe(el));
 
-if(document.URL.includes("invite")){
+if(document.URL.includes("invite")) {
     let counter = 3;
     setInterval(() => {
         counter > 0 ? counter-- : null;
-        counter == 0 ? window.location.replace("https://discord.com/oauth2/authorize?client_id=712759741528408064&permissions=275415092224&scope=applications.commands%20bot") : null;
-        counter == 0 ? document.getElementById("notRedirected").style.setProperty('opacity', '100%') : null;
+        counter === 0 ? window.location.replace("https://discord.com/oauth2/authorize?client_id=712759741528408064&permissions=275415092224&scope=applications.commands%20bot") : null;
+        counter === 0 ? document.getElementById("notRedirected").style.setProperty('opacity', '100%') : null;
 
         document.getElementById("counterElement").style.setProperty('--value', counter);
     }, 1000);
@@ -26,14 +26,14 @@ if(document.URL.includes("plugin")){
     let counter = 3;
     setInterval(() => {
         counter > 0 ? counter-- : null;
-        counter == 0 ? window.location.replace("https://www.spigotmc.org/resources/discord-linker.98749/") : null;
-        counter == 0 ? document.getElementById("notRedirected").style.setProperty('opacity', '100%') : null;
+        counter === 0 ? window.location.replace("https://www.spigotmc.org/resources/discord-linker.98749/") : null;
+        counter === 0 ? document.getElementById("notRedirected").style.setProperty('opacity', '100%') : null;
 
         document.getElementById("counterElement").style.setProperty('--value', counter);
     }, 1000);
 }
 
-document.querySelectorAll("[include-html]").forEach(async e => {
-    const r = await fetch(e.getAttribute("include-html"));
+document.querySelectorAll("[data-include-html]").forEach(async e => {
+    const r = await fetch(e.getAttribute("data-include-html"));
     e.innerHTML = await r.text();
 });

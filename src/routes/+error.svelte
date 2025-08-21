cedeb8957bfeabb3d1dbcf3f8c39859a500b902a
TheAnnoying <drork2018@gmail.com>
2025-08-21 22:16:49 +0300

    impr: removed back button (no where on the site where an error page is accesible, thus nothing to go back to)

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -11,10 +11,6 @@
 </svelte:head>
 
 <div in:fly={{ y: -5 }} class="p-5 prose dark:prose-invert prose-zinc !max-w-4xl">
-    <Button variant="ghost" class="mb-2" onclick={() => window.history.back()}>
-        <CornerUpLeft />
-        Back
-    </Button>
     <h1>{page.status} {page.error?.message}</h1>
-    The page `{page.url.pathname}` was not found
+    The page <code>{page.url.pathname}</code> was not found
 </div>

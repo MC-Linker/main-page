4fc2b2d1a82a9c8dbf593051af6180f6efe517a6
TheAnnoying <drork2018@gmail.com>
2025-08-21 22:04:42 +0300

    design: removed transition from component directly

--- src/lib/components/hero.svelte
+++ src/lib/components/hero.svelte
@@ -1,9 +1,8 @@
 <script lang="ts">
-    import { fly } from "svelte/transition";
     let { image, alt, title, description, link, reverse = false, order = 1 } = $props();
     import Button from "$lib/components/ui/button/button.svelte";
 </script>
-<div class="flex flex-col {reverse ? 'md:flex-row-reverse' : 'md:flex-row'} gap-10 items-center" in:fly={{ y: 10, delay: 200*order }}>
+<div class="flex flex-col {reverse ? 'md:flex-row-reverse' : 'md:flex-row'} gap-10 items-center">
     <img src={image} {alt}>
     <div class="flex flex-col gap-3">
         <h1 class="text-3xl font-bold">{title}</h1>
